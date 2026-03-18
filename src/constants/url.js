9cf507609c63cab497319c7877be2d7fc2148def
neeraj gautam <neerajgautam@gmail.com>
2026-03-18 11:27:07 +0530

    add update commit

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,5 +1,5 @@
 const isProduction = import.meta.env.VITE_IS_PRODUCTION === "false";
 
 export const API_URL = isProduction
-  ? ""
+  ? import.meta.env.VITE_API_PROD_URL
   : import.meta.env.VITE_API_URL;
