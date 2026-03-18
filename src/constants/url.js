91fa1e76ce84608023e76d452702875090bbc9f9
neeraj gautam <neerajgautam@gmail.com>
2026-03-18 11:42:02 +0530

    add all folder

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,4 @@
-const isProduction = import.meta.env.VITE_IS_PRODUCTION === "false";
+const isProduction = import.meta.env.VITE_IS_PRODUCTION === "true";
 
 export const API_URL = isProduction
   ? import.meta.env.VITE_API_PROD_URL
